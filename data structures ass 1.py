f41251d9bdf38f3f8877b2beef2846a11e47be0b
TejaSreeCheguri <87237462+TejaSreeCheguri@users.noreply.github.com>
2021-07-10 23:47:34 +0530

    Update data structures ass 1.py

--- data structures ass 1.py
+++ data structures ass 1.py
@@ -16,7 +16,7 @@ print("\n\n\n")
 fn= input("Enter Filename: ")
 #splitting the file name wherever "." is found
 f = fn.split(".")
-#we are printing f[-1] because, we know that the last word is the file extension
-#because there might be chances where we can have "." in the file name itself.
-#so it is safe to use f[-1] than f[1]
+""" we are printing f[-1] because, we know that the last word is the file extension
+ because there might be chances where we can have "." in the file name itself.
+so it is safe to use f[-1] than f[1]"""
 print ("Extension of the file is : " + f[-1])

66e85d5af13ab34b816e61866286f9b4ee96e4bd
TejaSreeCheguri <87237462+TejaSreeCheguri@users.noreply.github.com>
2021-07-10 23:46:50 +0530

    Add files via upload

---
+++ data structures ass 1.py
@@ -0,0 +1,22 @@
+
+#Task-1
+
+rad = input("input radius of the circle: ")
+rad = float(rad)
+pi = 3.14159
+area = pi*rad*rad
+print("The area of the circle with radius", rad , "is", area)
+
+
+
+
+#Task - 2
+
+print("\n\n\n")
+fn= input("Enter Filename: ")
+#splitting the file name wherever "." is found
+f = fn.split(".")
+#we are printing f[-1] because, we know that the last word is the file extension
+#because there might be chances where we can have "." in the file name itself.
+#so it is safe to use f[-1] than f[1]
+print ("Extension of the file is : " + f[-1])
